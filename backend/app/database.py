9710f46557bd3af5cc7440bfad76f034b6869dbb
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2025-09-23 19:12:50 -0500

    add test

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
 # Crear engine de SQLAlchemy
@@ -9,7 +8,7 @@ engine = create_engine(settings.database_url, echo=True)
 # Crear SessionLocal para manejar sesiones de base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base declarativa para los modelos
+# Base declarativa para los modelos (versión moderna)
 Base = declarative_base()
 
 # Dependencia para obtener la sesión de base de datos
