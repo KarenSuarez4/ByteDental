a1bc35471bb7d16ce07b169a19fc45b927d1881a
Lunna Sosa <lunna.sosa@uptc.edu.co>
2025-10-12 02:31:40 -0500

    Rename the ‘price’ field to ‘value’ in the DentalService model and update the __repr__ method to reflect the change.

--- backend/app/models/dental_service_models.py
+++ backend/app/models/dental_service_models.py
@@ -9,11 +9,11 @@ class DentalService(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
-    price = Column(Numeric(10, 2), nullable=False)
+    value = Column(Numeric(10, 2), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
 
     # Relación con Treatment
     treatments = relationship("Treatment", back_populates="dental_service")
 
     def __repr__(self):
-        return f"<DentalService(id={self.id}, name={self.name}, price={self.price}, is_active={self.is_active})>"
+        return f"<DentalService(id={self.id}, name={self.name}, value={self.value}, is_active={self.is_active})>"
